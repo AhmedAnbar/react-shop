88bc3319ec3f5f9c5c6b745d1d8e3b593226d4e6
Ahmed Anber <begnulinux@gmail.com>
2019-09-23 20:40:42 +0200

    Updates

---
+++ precache-manifest.9d51859ac6b2e3bcce98e9fb7f5c2362.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f1282da999fa30a54610a31c33f48e",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "cebd2639d17ae77882ed",
+    "url": "/crwn-clothing/static/css/main.8a3253ec.chunk.css"
+  },
+  {
+    "revision": "6a8b20e83b558d613124",
+    "url": "/crwn-clothing/static/js/2.f64de217.chunk.js"
+  },
+  {
+    "revision": "cebd2639d17ae77882ed",
+    "url": "/crwn-clothing/static/js/main.c751d980.chunk.js"
+  },
+  {
+    "revision": "e88d177d5d18f16bee32",
+    "url": "/crwn-clothing/static/js/runtime~main.675f06d2.js"
+  },
+  {
+    "revision": "3589bddbe338e444d408f4dbc545ca1e",
+    "url": "/crwn-clothing/static/media/OpenSansCondensed-Light.3589bddb.ttf"
+  },
+  {
+    "revision": "47602a5bb966f6fd33f8ceba8a4a8b2a",
+    "url": "/crwn-clothing/static/media/OpenSansCondensed-Light.47602a5b.eot"
+  },
+  {
+    "revision": "7b462187e2a7e7511f4c21abb30b0050",
+    "url": "/crwn-clothing/static/media/OpenSansCondensed-Light.7b462187.svg"
+  },
+  {
+    "revision": "850813cc33ae77cc148dd2fa623b5b00",
+    "url": "/crwn-clothing/static/media/OpenSansCondensed-Light.850813cc.woff2"
+  },
+  {
+    "revision": "c15a63a43d5a3c3b70bef29bdbbd27de",
+    "url": "/crwn-clothing/static/media/OpenSansCondensed-Light.c15a63a4.woff"
+  }
+]);
