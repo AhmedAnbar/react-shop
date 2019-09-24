3208b9fb00e0aeab3d049efe18807f2ed6c83e3d
Ahmed Anber <begnulinux@gmail.com>
2019-09-24 05:18:54 +0200

    Updating app with shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import './fonts/fonts.css'
 import HomePage from './pages/homepage/HomePage'
-
-const HatsPage = (props) => {
-  console.log(props)
-  return <h1>Hats Page</h1>
-}
+import ShopPage from './pages/shop/Shop'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
